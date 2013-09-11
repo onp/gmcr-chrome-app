8c7d311f836a6e6742af028039c60083fe748fff
onp <onptrs@gmail.com>
2013-09-11 10:36:07 -0400

    Load new conflict into main window

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -1,6 +1,15 @@
 $(function(){
-    
+    $("div#welcome-new").click(function(){
+        /*$.get("loadTarget.html",function(data){
+            $("div#content-main").html(data);
+            console.log("done");
+        });  */
+        $("div#main-content").html(templates.dmEditMain);
+        $("div#topbar-content").html(templates.dmEditTop);
+        dmPageScript();
 
+
+    });
 
 
 })

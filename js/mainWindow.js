4cd3e9d1284889e15614721f3510d0411d29e052
Oskar Petersons <a.more.reliable.source@gmail.com>
2013-09-12 23:28:38 -0400

    various changes
    add examples button to welcome page, activate "new" button on sidebar,
    fix presentation of topbar in a conflict.

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -1,15 +1,20 @@
 $(function(){
     $("div#welcome-new").click(function(){
-        /*$.get("loadTarget.html",function(data){
-            $("div#content-main").html(data);
-            console.log("done");
-        });  */
         $("div#main-content").html(templates.dmEditMain);
         $("div#topbar-content").html(templates.dmEditTop);
         dmPageScript();
-
-
+    });
+    $("p#button-new").click(function(){
+        $("div#main-content").html(templates.dmEditMain);
+        $("div#topbar-content").html(templates.dmEditTop);
+        dmPageScript();
     });
 
 
 })
+
+
+        /*$.get("loadTarget.html",function(data){
+            $("div#content-main").html(data);
+            console.log("done");
+        });  */

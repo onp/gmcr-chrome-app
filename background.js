3e3e3f6e7ea39323b5cedde64d3ce538ad94d274
Oskar Petersons <a.more.reliable.source@gmail.com>
2013-09-28 18:33:17 -0400

    increase default window size

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function(){
     chrome.app.window.create('window.html',{
         'bounds':{
             'width':900,
-            'height':500
+            'height':900
         }
     });
 });
